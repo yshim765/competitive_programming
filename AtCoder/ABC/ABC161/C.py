9d6975258220612212a594d65e441803e2163450
yshim765 <yshimizu765@gmail.com>
2020-04-04 22:49:28 +0900

    modify solution

--- AtCoder/ABC/ABC161/C.py
+++ AtCoder/ABC/ABC161/C.py
@@ -7,11 +7,8 @@ n, m = int(n), int(m)
 n, k = input().split()
 n, k = int(n), int(k)
 
-if abs(n - k) > n:
+n = n % k
+if n < k - n:
     print(n)
 else:
-    n = n % k
-    if n < k - n:
-        print(n)
-    else:
-        print(k - n)
+    print(k - n)
