b60d6ae0d0a6f885400488948c6e255f5b3f0671
yshim765 <yshimizu765@gmail.com>
2020-04-12 23:51:10 +0900

    add gcd func

--- AtCoder/ABC/utility/utility.py
+++ AtCoder/ABC/utility/utility.py
@@ -11,3 +11,12 @@ num_dict = dict(collections.Counter(temp))
 print(num_dict)
 
 # %%
+# 2つの数の最大公約数(greatest common divisor)を求める
+
+def gcd(a, b):
+    if (b == 0):
+        return(a)
+    
+    return gcd(b, a % b)
+
+# 3つの場合でも、gcd(a, gcd(b, c))みたいにして求める。3つ以上でも同様。
