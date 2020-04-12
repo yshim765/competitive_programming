01e8b5e5f28f3bd0b661005d7239dc8636a333f1
yshim765 <yshimizu765@gmail.com>
2020-04-12 20:56:46 +0900

    modify template

--- AtCoder/ABC/ABC_template/E.py
+++ AtCoder/ABC/ABC_template/E.py
@@ -2,7 +2,32 @@
 n, m = input().split()
 n, m = int(n), int(m)
 
+x = input().split()
+x = [int(X) for x in x]
+
 #%%
 
-n, m = input().split()
+input_string = \
+"""
+4 3
+1 2 3
+4 5 6
+7 8 9
+10 11 12
+"""
+
+input_string_list = input_string.split("\n")[1:-1]
+
+n, m = input_string_list[0].split()
 n, m = int(n), int(m)
+
+x = []
+
+for i in range(n):
+    x.append(input_string_list[i + 1].split())
+    x[i] = [int(x) for x in x[i]]
+
+print(n, m)
+print(x)
+
+# %%
