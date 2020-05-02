0b3e87b6118618be82573cd8370573ffbe99f7cd
yshim765 <yshimizu765@gmail.com>
2020-05-03 00:35:54 +0900

    add combinations_with_replacement func

--- AtCoder/ABC/utility/utility.py
+++ AtCoder/ABC/utility/utility.py
@@ -12,6 +12,8 @@ print(num_dict)
 
 # %%
 # 2つの数の最大公約数(greatest common divisor)を求める
+# 3つの場合でも、gcd(a, gcd(b, c))みたいにして求める
+# 3つ以上でも同様
 
 def gcd(a, b):
     if (b == 0):
@@ -19,8 +21,6 @@ def gcd(a, b):
     
     return gcd(b, a % b)
 
-# 3つの場合でも、gcd(a, gcd(b, c))みたいにして求める
-# 3つ以上でも同様
 
 # %%
 # 2つの数の最小公倍数(least common multiple)を求める
@@ -34,3 +34,19 @@ def gcd(a, b):
 
 def lcm(a, b):
     return a * b / gcd(a, b)
+
+# %%
+# 重複組み合わせを全列挙する
+# [1, 1, 1], [1, 1, 2], [1, 1, 3], [1, 2, 2], [1, 2, 3],
+# [1, 3, 3], [2, 2, 2] ... 的な奴
+
+import itertools
+
+# 長さ3, 最大5 の組み合わせを生成
+n = 3
+m = 5
+
+for l in itertools.combinations_with_replacement([x for x in range(1, m + 1)], n):
+    print(l)
+
+# %%
