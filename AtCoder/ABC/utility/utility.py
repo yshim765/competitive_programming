0ae9f326dcebac153733b68f0609b1f3c0d2ffb2
yshim765 <yshimizu765@gmail.com>
2020-05-31 00:25:40 +0900

    add cumsum reverse_cumsum

--- AtCoder/ABC/utility/utility.py
+++ AtCoder/ABC/utility/utility.py
@@ -50,3 +50,31 @@ for l in itertools.combinations_with_replacement([x for x in range(1, m + 1)], n
     print(l)
 
 # %%
+# cumsum
+# それまでの総和を求める
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] -> cumsum_l = [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+cumsum_l = l.copy()
+
+for i in range(len(l) - 1):
+    cumsum_l[i + 1] += cumsum_l[i]
+
+print(cumsum_l)
+
+#%%
+# reverse_cumsum
+# それ以降の総和を求める
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] -> reverse_cumsum_l = [55, 54, 52, 49, 45, 40, 34, 27, 19, 10]
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+reverse_cumsum_l = l.copy()
+
+for i in range(len(l)-2, -1, -1):
+    reverse_cumsum_l[i] += reverse_cumsum_l[i+1]
+
+print(reverse_cumsum_l)
+
+# %%
